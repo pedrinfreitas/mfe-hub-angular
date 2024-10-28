ee7d2f3e7d3601434aa290e4001e55ffbbf787fc
pedrinfreitas <pedrinfreitas@gmail.com>
2024-10-28 20:42:38 -0300

    iniciando o projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+
+  // {
+  //   path: 'faq',
+  //   loadChildren: () => import('mfe-faq/Module').then((m) => m.FaqModule),
+  // },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
